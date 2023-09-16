3379d62f2292845a6d94b6b28d8a3c5684b6306b
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-16 11:51:15 +0300

    Add files via upload

--- Extensions.cs
+++ Extensions.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using News.Models;
 
-namespace News.Extensions
+namespace Haberler.Extensions
 {
     public static class Extensions
     {
+        public static int GetNewsTitleId(string res)
+        {
+            if (res==null)
+            {
+                return 0;
+            }
+            string result = "";
+            for (int i = res.Length-1; i >0 ; i--)
+            {
+                if (res[i] >= 48 && res[i] <= 57)
+                {
+                    result += res[i];
+                }
+                if (res[i] == '-')
+                {
+                    string reverse = "";
+                    for (int j = result.Length-1; j >= 0; j--)
+                    {
+                        reverse +=result[j];
+                    }
+                    return Convert.ToInt32(reverse);
+                }
+            }
+            return 0;
+        }
+
         public static string ControlledSub(this string value, int lenght, string deVal)
         {
             if (string.IsNullOrWhiteSpace(value))
@@ -38,7 +61,7 @@ namespace News.Extensions
                     s += "<option value ='" + item.Value + "'>" + item.Text + "</option>";
             }
             //"JobState(" + item.Id.ToString() + ",'" + item.Status + "')"
-            string result = @"<select onchange="+ "NewsState(" + name + ",'" + select + "')" + @" class='form-control' style='height:31px;' data-val='true' id='NewsId" + name + @"' name='NewsId" + name + @"'>" + s + 
+            string result = @"<select onchange="+ "NewsState(" + name + ")" + @" class='form-control' style='height:31px;' data-val='true' id='NewsId" + name + @"' name='NewsId" + name + @"'>" + s + 
              @"</select>";
             return MvcHtmlString.Create(result);
         }
---
+++ ImageUpload.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DAL;
+
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+
+namespace Haberler.Extensions
+{
+    public static class ImageUpload
+    {
+        public static int ImageAddEditor(HttpPostedFileBase ImagePost)
+        {
+            string FilePath = "";
+            string Name = "";
+            if (ImagePost != null)
+            {
+                ImageRepository imageRepository = new ImageRepository();
+                Name = Guid.NewGuid().ToString() + "_" + Path.GetFileName(ImagePost.FileName);
+                FilePath = Path.Combine(HttpContext.Current.Server.MapPath("~/Images"), Name);
+                ImagePost.SaveAs(FilePath);
+                imageRepository.UploadImageInDataBase(new Image() { Name = Name, FileUrl = FilePath });
+                return imageRepository.List().FirstOrDefault(x => x.Name == Name).Id;
+            }
+            return 1;
+        }
+
+        public static int ImageAddNews(HttpPostedFileBase ImagePost)
+        {
+            string FilePath = "";
+            string Name = "";
+            if (ImagePost != null)
+            {
+                ImageRepository imageRepository = new ImageRepository();
+                Name = Guid.NewGuid().ToString() + "_" + Path.GetFileName(ImagePost.FileName);
+                FilePath = Path.Combine(HttpContext.Current.Server.MapPath("~/Images"), Name);
+                ImagePost.SaveAs(FilePath);
+                imageRepository.UploadImageInDataBase(new Image() { Name = Name, FileUrl = FilePath });
+                return imageRepository.List().FirstOrDefault(x => x.Name == Name).Id;
+            }
+            return 2;
+        }
+
+        public static void ImageRemove(string name)
+        {
+            File.Delete(HttpContext.Current.Server.MapPath("~/Images/") + name);
+        }
+
+        public static void ImageRemove(List<string> name)
+        {
+            foreach (var item in name)
+            {
+                File.Delete(HttpContext.Current.Server.MapPath("~/Images/") + item);
+            }
+        }
+    }
+}
